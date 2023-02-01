40d6111c80c5526418bf5f4f1781c1f223dc17a3
Philippe <44789132+PhilippeduPreez@users.noreply.github.com>
2023-02-01 12:14:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,56 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-import mailchimp from "@mailchimp/mailchimp_marketing";
-
-const MAILCHIMP_API_KEY = "ab8f895310006feb7d104d1516f43bc7-us21";
-const MAILCHIMP_API_SERVER = "us21";
-const MAILCHIMP_AUDIENCE_ID = "fba5e5d181";
-
-mailchimp.setConfig({
-  apiKey: MAILCHIMP_API_KEY,
-  server: MAILCHIMP_API_SERVER, // e.g. us1
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
 });
 
-
-export default async (req, res) => {
-  const { email } = req.body;
-  // Add headers to the response
-
-  if (!email) {
-    return res.status(400).json({ error: "Email is required" });
+async function handleRequest(request) {
+  if (request.method === "OPTIONS") {
+    let headers = new Headers();
+    headers.append("Access-Control-Allow-Origin", "*");
+    headers.append(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    headers.append(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Requested-With"
+    );
+    return new Response(null, { headers });
   }
 
-  try {
-    await mailchimp.lists.addListMember(MAILCHIMP_AUDIENCE_ID, {
-      email_address: email,
-      status: "subscribed",
-    });
-  } catch (error) {
-    return res.status(500).json({ error: error.message || error.toString() });
+  const data = await request.json();
+  const { email } = data;
+  
+  // Use Mailchimp API to add the email to the mailing list
+  // You will need to import the `fetch` function and pass in your API key and the list ID
+  const response = await fetch(
+    MAILCHIMP_URL,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `apikey ${API_KEY}`,
+      },
+      body: JSON.stringify({
+        email_address: email,
+        status: "subscribed",
+      }),
+    }
+  );
+
+  // Return a response indicating whether the email signup was successful or not
+  if (response.ok) {
+    return new Response(
+      JSON.stringify({ message: "Thank you for signing up!" }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } else {
+    return new Response(
+      JSON.stringify({ message: "An error occurred. Please try again later." }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
-};
+}
